c9e515fe0751fc1b9d0cdf988b87f9f484f2ab2b
Christian Karrasch <Christian.Karrasch@edu.bib.de>
2019-05-24 03:23:31 -0700

    JSON Abfrage implementiert

--- heldentool/ViewController.swift
+++ heldentool/ViewController.swift
@@ -16,6 +16,34 @@ class ViewController: UIViewController {
         // und los gehts mit dem tollen Heldentool
     }
 
+    // Methode um den Stammdaten JSON abzufragen
+    func loadBaseData(){
+        
+        //1 Datenquelle definieren:
+        let URLString = "http://pbs2h17aka.web.pb.bib.de/jsonExample.php"
+        let session = URLSession(configuration: URLSessionConfiguration.default)
+        let searchURL = URL(string: URLString)
+        let urlRequest = URLRequest(url: searchURL!)
+        
+        //2 Task definieren zum Laden, wird parallel zum weiteren Geschehen ausgeführt:
+        let dataTask = session.dataTask(with: urlRequest, completionHandler: { (data, response, error) in
+            if error != nil {
+                print("Fehler beim Laden: " + error!.localizedDescription)
+            }
+            else
+            {
+                //das wird ausgeführt, wenn die Daten vollständig geladen wurden
+                //self.datenParsenUndVerarbeiten (data: data!)
+            }
+        })
+        
+        //3 Task (also den Download) starten
+        dataTask.resume()
+    }
+    
+    func parseBaseData(data:Data?){
+        print("Oberst von Bratwurst grüßt die Männer von der Fritten Braterei!")
+    }
 
 }
 
