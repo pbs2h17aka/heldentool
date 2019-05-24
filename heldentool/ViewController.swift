0f69f5afd0fbf8df0165b36e784c10e79f236991
Christian Karrasch <Christian.Karrasch@edu.bib.de>
2019-05-24 03:13:23 -0700

    test commit

--- heldentool/ViewController.swift
+++ heldentool/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // und los gehts mit dem tollen Heldentool
     }
 
 
