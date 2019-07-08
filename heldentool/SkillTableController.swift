fc9cf8fbe8f74cc249459f4fa16e7f3800330e59
Christian Karrasch <Christian.Karrasch@edu.bib.de>
2019-07-08 07:37:28 -0700

    - Plus/Minus Knöpfe

--- heldentool/SkillTableController.swift
+++ heldentool/SkillTableController.swift
@@ -15,6 +15,66 @@ class SkillTableController: UITableViewController {
     // Core Data Model
     let heldenModel = HeldenModel.item
 
+    // Klick auf dem Minus Button
+    @IBAction func onMinus(_ sender: UIButton) {
+        let index = sender.tag
+        switch index {
+            case 0:
+                SharedItem.meinHeld.athletik -= 1
+            case 1:
+                SharedItem.meinHeld.gesellschaft -= 1
+            case 2:
+                SharedItem.meinHeld.handwerk -= 1
+            case 3:
+                SharedItem.meinHeld.heimlichkeit -= 1
+            case 4:
+                SharedItem.meinHeld.magie -= 1
+            case 5:
+                SharedItem.meinHeld.medizin -= 1
+            case 6:
+                SharedItem.meinHeld.waffen -= 1
+            case 7:
+                SharedItem.meinHeld.wildnis -= 1
+            case 8:
+                SharedItem.meinHeld.wissen -= 1
+            case 9:
+                SharedItem.meinHeld.wunder -= 1
+            default: print("Fehler in Minus Click Event")
+        }
+        
+        //self.tableView.reloadData()
+    }
+    
+    // Klick auf den Plus Button
+    @IBAction func onPlus(_ sender: UIButton) {
+        let index = sender.tag
+        switch index {
+        case 0:
+            SharedItem.meinHeld.athletik += 1
+        case 1:
+            SharedItem.meinHeld.gesellschaft += 1
+        case 2:
+            SharedItem.meinHeld.handwerk += 1
+        case 3:
+            SharedItem.meinHeld.heimlichkeit += 1
+        case 4:
+            SharedItem.meinHeld.magie += 1
+        case 5:
+            SharedItem.meinHeld.medizin += 1
+        case 6:
+            SharedItem.meinHeld.waffen += 1
+        case 7:
+            SharedItem.meinHeld.wildnis += 1
+        case 8:
+            SharedItem.meinHeld.wissen += 1
+        case 9:
+            SharedItem.meinHeld.wunder += 1
+        default: print("Fehler in Minus Click Event")
+        }
+        
+        //self.tableView.reloadData()
+    }
+    
     override func viewDidLoad() {
         tableView.tableFooterView = UIView()
         
@@ -47,8 +107,15 @@ class SkillTableController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "skillCell", for: indexPath)
-        let text : String
+        //let text : String
         
+        let name = cell.contentView.viewWithTag(1) as! UILabel
+        let wert = cell.contentView.viewWithTag(2) as! UILabel
+        
+        let minus = cell.contentView.viewWithTag(3) as! UIButton
+        let plus = cell.contentView.viewWithTag(4) as! UIButton
+        
+        /*
         switch indexPath.row {
         case 0: text = "Athletik: \(SharedItem.meinHeld.athletik)"
         case 1: text = "Gesellschaft: \(SharedItem.meinHeld.gesellschaft)"
@@ -62,8 +129,53 @@ class SkillTableController: UITableViewController {
         case 9: text = "Wunder: \(SharedItem.meinHeld.wunder)"
         default: text = "yee"
         }
+         */
         
-        cell.textLabel?.text = text
+        switch indexPath.row {
+        case 0: name.text = "Athletik:"
+                wert.text = String(SharedItem.meinHeld.athletik)
+                minus.tag = indexPath.row
+                plus.tag = indexPath.row
+        case 1: name.text = "Gesellschaft:"
+                wert.text = String(SharedItem.meinHeld.gesellschaft)
+                minus.tag = indexPath.row
+                plus.tag = indexPath.row
+        case 2: name.text = "Handwerk:"
+                wert.text = String(SharedItem.meinHeld.handwerk)
+                minus.tag = indexPath.row
+                plus.tag = indexPath.row
+        case 3: name.text = "Heimlichkeit:"
+                wert.text = String(SharedItem.meinHeld.heimlichkeit)
+                minus.tag = indexPath.row
+                plus.tag = indexPath.row
+        case 4: name.text = "Magie:"
+                wert.text = String(SharedItem.meinHeld.magie)
+                minus.tag = indexPath.row
+                plus.tag = indexPath.row
+        case 5: name.text = "Medizin:"
+                wert.text = String(SharedItem.meinHeld.medizin)
+                minus.tag = indexPath.row
+                plus.tag = indexPath.row
+        case 6: name.text = "Waffen:"
+                wert.text = String(SharedItem.meinHeld.waffen)
+                minus.tag = indexPath.row
+                plus.tag = indexPath.row
+        case 7: name.text = "Wildnis:"
+                wert.text = String(SharedItem.meinHeld.wildnis)
+                minus.tag = indexPath.row
+                plus.tag = indexPath.row
+        case 8: name.text = "Wissen:"
+                wert.text = String(SharedItem.meinHeld.wissen)
+                minus.tag = indexPath.row
+                plus.tag = indexPath.row
+        case 9: name.text = "Wunder:"
+                wert.text = String(SharedItem.meinHeld.wunder)
+                minus.tag = indexPath.row
+                plus.tag = indexPath.row
+        default: name.text = "yee"
+        }
+        
+        //cell.textLabel?.text = text
         
         return cell
     }
@@ -74,5 +186,7 @@ class SkillTableController: UITableViewController {
         heldenModel.sendeHelden()
         SharedItem.meinHeld.speicherHelden()
     }
+    
+    
 
 }
